99790c81bfc04ce7dafcd156655fec594a56e64a
omar2205 <omar2205a@yahoo.com>
2019-02-10 22:23:45 +0200

    update 3

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 window.onbeforeunload = function() { return "Would you really like to close your browser?"; }
 document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM fully loaded and parsed");
-
     if (sessionStorage.getItem('mode')) {
         var mode = sessionStorage.getItem('mode')
         if (mode == "dark") {
